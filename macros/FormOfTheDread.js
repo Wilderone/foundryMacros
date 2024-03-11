4578edd995a0a0cdb63155f6b43e2d35c25930a3
Max Wilde <prim@MacBook-Pro.local>
2024-03-11 22:31:46 +0100

    add randomizer

--- macros/FormOfTheDread.js
+++ macros/FormOfTheDread.js
@@ -3,6 +3,54 @@ const updates = [];
 let normal = 'worlds/forgotten-advisor-of-luthard/characters/041_-_A_27yat_Nemerym/sides/side_0.png';
 let undead = 'worlds/forgotten-advisor-of-luthard/characters/041_-_A_27yat_Nemerym/sides/side_1.png';
 
+const randomItem = (array) => array[Math.floor(Math.random() * array.length)];
+
+const eyes = [
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Few01_01_Dark_Purple_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Few01_01_Dark_Red_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Few01_01_Regular_OrangeRed_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Few01_02_Dark_Red_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Few01_02_Regular_OrangeRed_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Few01_03_Dark_Red_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Many01_01_Dark_Red_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Many01_02_Dark_Red_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Many01_02_Regular_OrangeRed_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Many01_03_Dark_Red_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Single01_01_Dark_Red_300x300.webm",
+  "modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Single01_01_Regular_OrangeRed_300x300.webm"
+]
+
+const impacts = [
+  "modules/jb2a_patreon/Library/Generic/Impact/Impact_02_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/Impact_03_Dark_Purple_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/Impact_03_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/Impact_04_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/Impact_07_Regular_Red02_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/Impact_11_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/Impact_12_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/Impact_12_Regular_Red_400x400.webm"
+]
+
+const lightnings = [
+  "modules/jb2a_patreon/Library/Generic/Lightning/LightningBall_01_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Lightning/LightningBall_01_Regular_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Lightning/StaticElectricity_01_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Lightning/StaticElectricity_01_Regular_Orange_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Lightning/StaticElectricity_02_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Lightning/StaticElectricity_03_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Lightning/StaticElectricity_03_Regular_Red_400x400.webm"
+]
+
+const shadows = [
+  "modules/jb2a_patreon/Library/Generic/Smoke/Fumes04_01_Black_Loop_400x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Smoke/SmokePlumes01_03_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Smoke/SmokePuff01_01_Dark_Black_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Smoke/SmokePuff01_01_Regular_Grey_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Smoke/SmokePuff01_02_Dark_Black_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Smoke/SmokePuffSide01_01_Dark_Black_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Smoke/SmokeRing_01_Dark_Black_800x800.webm"
+]
+
 // ID of the token that used by the item
 let eventToken =  canvas.tokens.get(args[1].tokenId);
 // ID of player token
@@ -14,27 +62,25 @@ if (args[0] == "on") // args is an array, zero element contains "on" or "off" (e
 {
     new Sequence() // This will apply animation
     .effect()
-      .file("modules/jb2a_patreon/Library/Generic/Eyes/Eyes_Few01_03_Dark_Red_600x600.webm")
+      .file(randomItem(eyes))
       .attachTo(eventToken)
-      .belowTokens()
       .filter("ColorMatrix", {hue:-15, saturate:1})
       .size(3, {gridUnits: true})
 
     .effect()
-      .file("modules/jb2a_patreon/Library/Generic/Impact/Impact_11_Dark_Red_400x400.webm")
+      .file(randomItem(lightnings))
       .attachTo(eventToken)
       .size(3, { gridUnits: true })
 
     .effect()
-      .file("modules/jb2a_patreon/Library/Generic/Lightning/StaticElectricity_01_Dark_Red_400x400.webm")
+      .file(randomItem(shadows))
       .attachTo(eventToken)
       .belowTokens()
       .size(3.5, {gridUnits: true})
 
     .effect()
-      .file("modules/jb2a_patreon/Library/Generic/Impact/DropShadow_01_Dark_Black_400x400.webm")
+      .file(randomItem(impacts))
       .attachTo(eventToken)
-      .belowTokens()
       .size(3.5, {gridUnits: true})
         .play();
 
--- macros/rage.js
+++ macros/rage.js
@@ -3,6 +3,41 @@ const updates = [];
 let normal = 'worlds/forgotten-advisor-of-luthard/characters/047_-__D0_9B_D1_83_D0_B1_D1_83_D1_88/sides/side_0.png';
 let enraged = 'worlds/forgotten-advisor-of-luthard/characters/047_-__D0_9B_D1_83_D0_B1_D1_83_D1_88/sides/side_1.png';
 
+const randomItem = (array) => array[Math.floor(Math.random() * array.length)];
+
+const explosions = [
+  "modules/jb2a_patreon/Library/Generic/Explosion/SideExplosion01_01_Regular_Orange_600x600.webm",
+  "modules/JB2A_DnD5e/Library/Generic/Explosion/Explosion_01_Orange_400x400.webm",
+  "modules/JB2A_DnD5e/Library/Generic/Explosion/Explosion_03_Regular_BlueYellow_400x400.webm",
+  "modules/JB2A_DnD5e/Library/Generic/Explosion/SideExplosion01_01_Regular_Orange_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_02_Orange_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_04_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_04_Regular_Orange_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Explosion/ShrapnelBomb01_01_Regular_Red_800x800.webm",
+]
+
+const pulses = [
+  "modules/jb2a_patreon/Library/TMFX/OutPulse/Circle/OutPulse_01_Circle_Normal_500.webm",
+  "modules/jb2a_patreon/Library/TMFX/OutPulse/Circle/OutPulse_02_Circle_Normal_500.webm",
+  "modules/jb2a_patreon/Library/TMFX/OutPulse/Circle/OutPulse_03_Circle_Normal_500.webm",
+  "modules/jb2a_patreon/Library/TMFX/OutFlow/Circle/OutFlow_02_Circle_500x500.webm",
+  "modules/jb2a_patreon/Library/TMFX/OutFlow/Circle/OutFlow_03_Circle_500x500.webm"
+]
+
+const impacts = [
+  "modules/jb2a_patreon/Library/Generic/Impact/GroundCrackImpact_01_Dark_Red_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/GroundCrackImpact_01_Regular_Blue_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/GroundCrackImpact_01_Regular_Green_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/GroundCrackImpact_01_Regular_White_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/GroundCrackImpact_01_StillFrame.webp",
+  "modules/jb2a_patreon/Library/Generic/Impact/GroundCrackImpact_02_Dark_Red_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/GroundCrackImpact_02_Regular_Orange_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/GroundCrackImpact_03_Dark_Red_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/GroundCrackImpact_03_Regular_White_600x600.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/Impact_04_Dark_Red_400x400.webm",
+  "modules/jb2a_patreon/Library/Generic/Impact/Impact_11_Regular_Red_400x400.webm"
+]
+
 // ID of the token that used the item
 let eventToken =  canvas.tokens.get(args[1].tokenId)
 let controlledToken = canvas.tokens.controlled[0];
@@ -11,20 +46,20 @@ if (args[0] == "on")
 {
     new Sequence()
     .effect()
-    .file("modules/jb2a_patreon/Library/Generic/Explosion/SideExplosion01_01_Regular_Orange_600x600.webm")
+    .file(randomItem(explosions))
     .attachTo(eventToken)
     .belowTokens()
     .filter("ColorMatrix", {hue:-15, saturate:1})
     .size(1.5, {gridUnits: true})
 
   .effect()
-    .file("modules/jb2a_patreon/Library/TMFX/OutPulse/Circle/OutPulse_02_Circle_Normal_500.webm")
+    .file(randomItem(pulses))
     .attachTo(eventToken)
     .size(4, { gridUnits: true })
     .opacity(0.25)
 
   .effect()
-    .file("modules/jb2a_patreon/Library/Generic/Impact/GroundCrackImpact_01_Regular_Orange_600x600.webm")
+    .file(randomItem(impacts))
     .attachTo(eventToken)
     .belowTokens()
     .filter("ColorMatrix", {hue:-15, saturate:1})
